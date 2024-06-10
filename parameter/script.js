e929f005087ca0884082847c09969c19650ef905
PP Pixel <134684159+PP-Pixel@users.noreply.github.com>
2024-06-11 00:37:08 +0900

    parameter

--- parameter/script.js
+++ parameter/script.js
@@ -1,14 +1,14 @@
 // URL のクエリパラメータを取得
 const urlParams = new URLSearchParams(window.location.search);
 
-// send パラメータの値を取得 (デフォルトは false)
-const sendValue = urlParams.get('send') === 'True';
+// param パラメータの値を取得 (デフォルトは false)
+const sendValue = urlParams.get('param') === 'True';
 
 // 要素を取得
 const trueContent = document.getElementById("true-content");
 const falseContent = document.getElementById("false-content");
 
-// send パラメータの値によって表示を切り替え
+// param パラメータの値によって表示を切り替え
 if (sendValue) {
     trueContent.classList.remove("hidden");
 } else {
