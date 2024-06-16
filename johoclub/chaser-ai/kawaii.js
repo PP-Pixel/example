9415c95acdd1432443d0a90a3780e67f1c2390cd
PP Pixel <134684159+PP-Pixel@users.noreply.github.com>
2024-06-16 23:36:38 +0900

    kawaii=False

---
+++ johoclub/chaser-ai/kawaii.js
@@ -0,0 +1,16 @@
+// // URL のクエリパラメータを取得
+// const urlParams = new URLSearchParams(window.location.search);
+
+// // kawaii パラメータの値を取得 (デフォルトは false)
+// const sendValue = urlParams.get('kawaii') === 'True';
+
+// // 要素を取得
+// const trueContent = document.getElementById("true-content");
+// const falseContent = document.getElementById("false-content");
+
+// // param パラメータの値によって表示を切り替え
+// if (sendValue) {
+//     trueContent.classList.remove("hidden");
+// } else {
+//     falseContent.classList.remove("hidden");
+// }
